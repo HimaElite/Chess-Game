f4bb73dac52d541fb0901177ecd18446b8520fc0
Ibrahim Belal <ibrahimbelal2005@gmail.com>
2025-12-16 01:24:35 +0200

    add comment

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ import random as rand
 #   ['q', 'quit', 'exit'] ---> to quit
 #   'fen' ---> to get the fen string of the board
 #   'undo' ---> to undo your last move
+#   'all moves' ---> it gives you all legal moves
 #   'moves position' ---> this will return avalable
 #                         moves for this position
 #   'position1 position2' ---> to move a piece from
@@ -100,7 +101,6 @@ def ai_move(game_board, undo_stack):
     else:
         the_move = f"AI played piece from {from_name} to {to_name}"
     return undo_stack, the_move
-
 
 def human_move(game_board, undo_stack, q, option=False):
     while True:
